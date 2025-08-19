825ed2eea89fbf5c4808cb9fc51570aa09486cbf
Hayden Sullivan <shsullivan2@gmail.com>
2025-08-18 22:22:10 -0400

    Added recursiveFibonnaci method and called in main

--- src/main/java/com/sullivan/fibonacciapp/FibonacciApp.java
+++ src/main/java/com/sullivan/fibonacciapp/FibonacciApp.java
@@ -1,7 +1,23 @@
 package com.sullivan.fibonacciapp;
 
 public class FibonacciApp {
-    public static void main(String[] args) {
 
+    public static int recursiveFibonacci(int n) {
+        // Base calculation case
+        if (n <= 0) {
+            return 0;
+        }
+        else if (n == 1) {
+            return 1;
+        }
+        // Recursive step
+        else {
+            return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 10;
+        System.out.println("The " + number + " term in the Fibonacci sequence is " + recursiveFibonacci(number));
     }
 }
